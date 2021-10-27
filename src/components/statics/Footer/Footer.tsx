f89685ba40ae6de8ede7454023337c247746dae9
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-10-27 13:09:13 -0300

    Feat.: Refatorando os Componentes Home, Navbar e Footer

--- src/components/statics/Footer/Footer.tsx
+++ src/components/statics/Footer/Footer.tsx
@@ -1,9 +1,48 @@
 import React from "react";
+import { Box } from "@material-ui/system";
+import { Grid, Typography } from "@material-ui/core";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Footer() {
     return (
         <>
-            <h1>footer</h1>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+
+                <Grid alignItems="center" item xs={12}>
+
+                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noreferrer">
+                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                            <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noreferrer">
+                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noreferrer">
+                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                        </Box>
+                    </Box>
+
+                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                        <Box paddingTop={1}>
+                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                        </Box>
+                        <Box>
+                            <a target="_blank" href="https://brasil.generation.org" rel="noreferrer">
+                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                            </a>
+                        </Box>
+                    </Box>
+
+                </Grid>
+
+            </Grid>
         </>
     )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,34 @@
 import React from "react";
+import { Button, Grid, Typography } from "@material-ui/core";
+import { Box } from "@material-ui/system";
+
 import './Home.css'
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-            <h1>Home</h1>
-            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img"/>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>
+                            Seja bem vinde!
+                        </Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>
+                            Expresse aqui os seus pensamentos e opiniões!
+                        </Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     )
 }

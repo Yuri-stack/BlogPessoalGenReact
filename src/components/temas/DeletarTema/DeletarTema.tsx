22b96ad7e71e84892d4c5d21af8f2c51263a5f7e
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-11-10 14:30:13 -0300

    Feat.: Implementando Delete de Tema

--- src/components/temas/DeletarTema/DeletarTema.tsx
+++ src/components/temas/DeletarTema/DeletarTema.tsx
@@ -3,11 +3,53 @@ import { Box, Card, CardActions, CardContent, Button, Typography } from '@materi
 import { useHistory, useParams } from 'react-router-dom';
 import useLocalStorage from 'react-use-localstorage';
 
+import { buscaId, deleteId } from "../../../services/Service";
 import Tema from '../../../models/Tema';
 
 import './DeletarTema.css';
 
 function DeletarTema() {
+    const { id } = useParams<{ id: string }>()
+    const [token, setToken] = useLocalStorage('token')
+    const [tema, setTema] = useState<Tema>()
+
+    let history = useHistory()
+
+    useEffect(() => {
+        if (token === '') {
+            alert("Você precisa estar logado")
+            history.push("/login")
+        }
+    }, [token])
+
+    useEffect(() => {
+        if (id !== '') {
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/tema/${id}`, setTema, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function sim() {
+        history.push('/temas')
+        deleteId(`/tema/${id}`, {
+            headers: {
+                'Authorization': token
+            }
+        });
+        alert('Tema deletado com sucesso');
+    }
+
+    function nao() {
+        history.push('/temas')
+    }
+
     return (
         <>
             <Box m={2}>
@@ -25,12 +67,12 @@ function DeletarTema() {
                     <CardActions>
                         <Box display="flex" justifyContent="start" ml={1.0} mb={2} >
                             <Box mx={2}>
-                                <Button variant="contained" className="marginLeft" size='large' color="primary">
+                                <Button onClick={sim} variant="contained" className="marginLeft" size='large' color="primary">
                                     Sim
                                 </Button>
                             </Box>
                             <Box mx={2}>
-                                <Button variant="contained" size='large' color="secondary">
+                                <Button onClick={nao} variant="contained" size='large' color="secondary">
                                     Não
                                 </Button>
                             </Box>

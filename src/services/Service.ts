9458471dd0cf9ccd7349bc8c2aa86df39b1485b2
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-11-10 12:06:58 -0300

    Feat.: Implementando o Post e Put de Tema

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -18,3 +18,22 @@ export const busca = async(url: string, setDados: any,  header: any) => {
     const resposta = await api.get(url, header)
     setDados(resposta.data)
 }
+
+export const buscaId = async(url: string, setDados: any,  header: any) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
+
+export const post = async(url: string, dados: any, setDados: any,  header: any) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const put = async(url: string, dados: any, setDados: any,  header: any) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const deleteId = async(url: string, header: any) => {
+    await api.delete(url, header)
+}
